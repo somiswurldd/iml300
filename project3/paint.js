99c3728b75f0edf797faaf443d4ff731c8876b13
somiswurldd <saumyaaw@usc.edu>
2022-11-28 23:56:25 -0800

    okay im getting somewhere

---
+++ project3/paint.js
@@ -0,0 +1,45 @@
+function setup() {
+  createCanvas(windowWidth,windowHeight);
+  distance = 10;
+  spring = 0.5;
+  friction = 0.5;
+  size = 10;
+  diff = size/8;
+  x = y = ax = ay = a = r = f = 0;
+}
+
+function draw() {
+  oldR = r;
+  if(mouseIsPressed) {
+    mX = mouseX;
+    mY = mouseY;
+    if(!f) {
+      f = 1;
+      x = mX;
+      y = mY;
+    }
+    ax += ( mX - x ) * spring;
+    ay += ( mY - y ) * spring;
+    ax *= friction;
+    ay *= friction;
+    a += sqrt( ax*ax + ay*ay ) - a;
+    a *= 0.6;
+    r = size - a;
+    
+    for( i = 0; i < distance; ++i ) {
+      oldX = x;
+      oldY = y;
+      x += ax / distance;
+      y += ay / distance;
+      oldR += ( r - oldR ) / distance;
+      if(oldR < 1) oldR = 1;
+      strokeWeight( oldR+diff );
+      line( x, y, oldX, oldY );
+      strokeWeight( oldR );
+      line( x+diff*2, y+diff*2, oldX+diff*2, oldY+diff*2 );
+      line( x-diff, y-diff, oldX-diff, oldY-diff );
+    }
+  } else if(f) {
+    ax = ay = f = 0;
+  }
+}
