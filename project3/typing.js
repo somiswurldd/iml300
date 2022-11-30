f4e12819115da5043126891f2b4284dea7ae4bb1
somiswurldd <saumyaaw@usc.edu>
2022-11-29 16:26:00 -0800

    create file wthout izp

--- project3/typing.js
+++ project3/typing.js
@@ -1,10 +1,6 @@
-var i = 0;
-var txt = 'Lorem ipsum typing effect!'; /* The text */
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-
-function typeWriter() {
-  if (i < txt.length) {
-    document.getElementById("demo").innerHTML += txt.charAt(i);
-    i++;
-    setTimeout(typeWriter, speed);
-  }
+$('h1').typeWrite({
+  speed: 30,
+  repeat: false,
+  cursor: true,
+  color: 'blue',
+})
